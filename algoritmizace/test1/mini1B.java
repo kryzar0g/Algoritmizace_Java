c150d88d4cdf59d913c2b913948a5d78c327521c
Krystof Zaruba <kryzar0g@gmail.com>
2025-10-08 12:47:11 +0200

    test1

---
+++ algoritmizace/test1/mini1B.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+public class mini1B {
+    
+    public static void main(String args[]){
+       int soucet_oken=0;
+    int max=0;
+    int umyto=0;
+    int pocet_brigadniku=0;
+    Scanner sc = new Scanner(System.in);
+    System.out.printf("Zadej pocet brigadniku:\t");
+    pocet_brigadniku = sc.nextInt();
+    for(int i = 0;i<pocet_brigadniku;i++){
+
+    System.out.printf("Zadej pocet oken umytych brigadnikem c. :%d\t",i+1);
+    umyto = sc.nextInt();
+    soucet_oken = soucet_oken + umyto;
+    if (umyto < 10){
+        System.out.println("malo vykonny brigadnik - vyradit!!!\n");
+    }
+    if (umyto > max){
+        max = umyto;}
+    }
+    System.out.printf("Celkem bylo umyto oken: %d\n",soucet_oken);
+    System.out.printf("jeden brigadnik dnes umyl maximalne %d oken\n",max);
+    sc.close();
+}
+
+
+    }
+    
+
